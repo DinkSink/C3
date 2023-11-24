e13659470e219047946bdde160745a2a9ca41725
Joshua Hillis <joshuahillis292002@tamu.edu>
2023-11-24 15:06:15 -0600

    Cross platform compatibility update!

--- importFromGhidra.py
+++ importFromGhidra.py
@@ -1,18 +1,24 @@
 import psutil
 import pyhidra
-import ghidra.app.script.GhidraScript
-import ghidra.program.model.data.StringDataType as StringDataType
-import exceptions
-
+#import ghidra.app.script.GhidraScript
+#import ghidra.program.model.data.StringDataType as StringDataType
+#import exceptions
 def is_ghidra_running():
-    # Get a list of all running processes
+    # Check for Ghidra in running processes
     for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-        print(process)
-        # Check if the process is 'javaw.exe' and contains 'ghidra' in its command line
-        if process.info['name'].lower() == 'javaw.exe' and 'ghidra' in ' '.join(process.info['cmdline']).lower():
-            return True
-    return False
+        # Process name normalization (lowercase)
+        process_name = process.info['name'].lower() if process.info['name'] else ""
 
+        # On Windows, Ghidra runs as 'javaw.exe', on Linux it's usually 'java'
+        valid_process_names = ['javaw.exe', 'java']
+
+        # Check if the process name is in the list of valid names
+        if any(proc_name in process_name for proc_name in valid_process_names):
+            # Check if 'ghidra' is in the command line arguments
+            cmdline = ' '.join(process.info['cmdline']).lower() if process.info['cmdline'] else ""
+            if 'ghidra' in cmdline:
+                return True
+    return False
 
 if is_ghidra_running():
     print("Ghidra is running.")
