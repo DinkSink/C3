3df1f32b468950777bc9adc292f936360e5c9136
KoryMW <korywelch7@gmail.com>
2023-11-29 22:18:24 -0600

    English Filter Changed to Function

--- EnglishFilter.py
+++ EnglishFilter.py
@@ -1,22 +1,27 @@
 import json
 import re
 
-file_path = "./strings_output_C2.json"
-with open(file_path, 'r') as file:
-    json_data = json.load(file)
-
-count = 0
-
-strings = []
-with open("./English.txt", 'r') as file:
-    for line in file:
-        strings.append(line.strip())
-
-for item in json_data["strings"]:
-    for s in strings:
-        if s in item["value"]:
-            print(item["value"], " + ", item["location"])
-    else:
-        count = count + 1    
+def find_english_words(file_path):
     
-print("Filtered out: " + str(count))
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+
+    strings = []
+    with open("./English.txt", 'r') as file:
+        for line in file:
+            strings.append(line.strip())
+            
+    found_words = []
+
+    for item in json_data["strings"]:
+        for s in strings:
+            if s in item["value"]:
+                found_words.append(item)
+    
+    return found_words
+
+file_path = "./strings_output_C2.json"
+found_words = find_english_words(file_path)
+print("English Words Found: ")
+for elem in found_words:
+    print(elem)
