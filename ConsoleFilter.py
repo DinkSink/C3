c21f89e6c061c4e57e75a710b3d1aceab2efb28f
KoryMW <korywelch7@gmail.com>
2023-11-29 21:59:33 -0600

    Console filter done?

--- ConsoleFilter.py
+++ ConsoleFilter.py
@@ -1,32 +1,48 @@
 import json
 import re
 
-file_path = "./strings_output.json"
+file_path = "./strings_output_C2.json"
 with open(file_path, 'r') as file:
     json_data = json.load(file)
 
 count = 0
 
 strings_list = json_data["strings"]
+
+linux_commands = []
+windows_commands = []
     
 with open("./linuxDictionary.txt", "r") as linux_Dict:
-    linux_commands = linux_Dict.readlines()
+    for line in linux_Dict.readlines():
+        linux_commands.append(line.strip())
     
 with open("./windowsDictionary.txt", "r") as windows_Dict:
-    windows_commands = windows_Dict.readlines()
-
+    for line in windows_Dict.readlines():
+        windows_commands.append(line.strip())
     
 found_linux_commands = []
 found_windows_commands = []
 
-for line in strings_list:
-    if (line["value"] in linux_commands) or (re.search(r" \/[^\/\s]+(?:\/[^\/\s]+)+", line["value"])):
-        found_linux_commands.append(line)
-        print(line)
-    elif (line["value"] in windows_commands) or (re.search(r"[a-zA-Z]:\\(?:[^\\\/\s]+\\)*[^\\\/\s]+\.(exe|bat|cmd|ps1)", line["value"])):
-        found_windows_commands.append(line)
-        print(line)
+for elem in strings_list:
+    if re.search(r" \/[^\/\s]+(?:\/[^\/\s]+)+", elem["value"]):
+        found_linux_commands.append(elem)
+        print(elem)
+        continue
+    if re.search(r"[a-zA-Z]:\\(?:[^\\\/\s]+\\)*[^\\\/\s]+\.(exe|bat|cmd|ps1)", elem["value"]):
+        found_windows_commands.append(elem)
+        print(elem)
+        continue
+    for line in linux_commands:
+        if ((line + " ") in elem["value"]):
+            found_linux_commands.append(elem)
+            print(elem)
+            break
+    for line in windows_commands:
+        if ((line + " ") in elem["value"]):
+            found_windows_commands.append(elem)
+            print(elem)
+            break
     else:
         count += 1
-
+        
 print("Filtered out: " + str(count))
