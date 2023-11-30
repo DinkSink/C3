44120e90fa25173e8f1b67498cdcfde4adba6ea7
Overgaard219 <cory.a.overgaard@gmail.com>
2023-11-29 21:34:31 -0600

    Filters the json now

--- CompilerFilter.py
+++ CompilerFilter.py
@@ -9,11 +9,9 @@ count = 0
 
 strings = []
 for item in json_data["strings"]:
-    strings.append(item["value"])
-
-for elem in strings:
-    if not re.search(r"_Z", elem) and not re.search(r"gxx", elem) and not re.search(r"GLIBC", elem) and not re.search(r"CXX", elem) and not re.search(r"GCC", elem) and not (elem == "None") and not re.search(r"align", elem):
-        print(elem)
+    if not re.search(r"_Z", item["value"]) and not re.search(r"gxx", item["value"]) and not re.search(r"GLIBC", item["value"]) and not re.search(r"CXX", item["value"]) and not re.search(r"GCC", item["value"]) and not (item["value"] == "None") and not re.search(r"align", item["value"]):
+        print(item["value"], " + ", item["location"])
     else:
-        count = count + 1
+        count = count + 1    
+    
 print("Filtered out: " + str(count))
